6adc459a7e23f40ed943f8c8a2ad4f195ef174b8
kn65op <199537+kn65op@users.noreply.github.com>
2020-05-02 16:14:19 +0200

    Add tests

--- android/app/src/androidTest/java/com/example/domag2/uitests/common/UiActions.kt
+++ android/app/src/androidTest/java/com/example/domag2/uitests/common/UiActions.kt
@@ -1,25 +1,16 @@
 package com.example.domag2.uitests.common
 
-import android.view.View
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.ViewInteraction
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.contrib.DrawerActions
+import androidx.test.espresso.contrib.NavigationViewActions
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.Espresso.onView
-
-import androidx.appcompat.widget.AppCompatImageButton
-
-import android.widget.ImageButton
-import androidx.appcompat.widget.Toolbar
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import com.example.domag2.R
-
-import org.hamcrest.CoreMatchers.allOf
-import org.hamcrest.Matchers.`is`
-import org.hamcrest.Matchers.anyOf
-import java.util.regex.Matcher
+import java.lang.Thread.sleep
 
 
 private fun clickOn(view: ViewInteraction) {
@@ -51,6 +42,27 @@ fun clickHomeIcon() {
     //onView(androidHomeMatcher()).perform(click())
     onView(withContentDescription(R.string.abc_action_bar_up_description)).perform(click());
 
+}
+
+fun openPart(part : String) {
+    onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+    onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_categories));
+    sleep(500)
+    /*
+    // Open Drawer to click on navigation.
+    onView(withId(R.id.drawer_layout))
+        .check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
+        .perform(DrawerActions.open()); // Open Drawer
+
+    // Start the screen of your activity.
+    onView(withId(R.id.nav_view))
+        .perform(NavigationViewActions.navigateTo(R.id.your_navigation_menu_item));
+
+    // Check that you Activity was opened.
+    String expectedNoStatisticsText = InstrumentationRegistry.getTargetContext()
+        .getString(R.string.no_item_available);
+    onView(withId(R.id.no_statistics)).check(matches(withText(expectedNoStatisticsText)));
+    */
 }
 
 /*
