8b9dcc9a09a90e4012f583c312b66a013bcaaec9
kn65op <199537+kn65op@users.noreply.github.com>
2020-05-26 20:16:30 +0200

    add best before

--- android/app/src/main/java/com/example/domag2/database/entities/Item.kt
+++ android/app/src/main/java/com/example/domag2/database/entities/Item.kt
@@ -3,6 +3,7 @@ package com.example.domag2.database.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import io.github.kn65op.android.lib.type.FixedPointNumber
+import java.time.ZonedDateTime
 
 @Entity
 data class Item(
@@ -11,9 +12,16 @@ data class Item(
     val amount: FixedPointNumber,
     val depotId: Int,
     val categoryId: Int,
-    val description: String? = null
+    val description: String? = null,
+    val bestBefore: ZonedDateTime? = null
 )
 
 fun Item.withDescription(newDescription: String) =
-    Item(uid = uid, depotId = depotId, categoryId = categoryId, description = newDescription, amount = amount)
+    Item(
+        uid = uid,
+        depotId = depotId,
+        categoryId = categoryId,
+        description = newDescription,
+        amount = amount
+    )
 
