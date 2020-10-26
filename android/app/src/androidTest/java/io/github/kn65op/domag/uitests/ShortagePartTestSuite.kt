9f3495bcb2f93c477cd1f5a241359f605339d17b
kn65op <199537+kn65op@users.noreply.github.com>
2020-10-26 19:14:14 +0100

    Add test

--- android/app/src/androidTest/java/io/github/kn65op/domag/uitests/ShortagePartTestSuite.kt
+++ android/app/src/androidTest/java/io/github/kn65op/domag/uitests/ShortagePartTestSuite.kt
@@ -3,11 +3,16 @@ package io.github.kn65op.domag.uitests
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import io.github.kn65op.android.lib.type.FixedPointNumber
 import io.github.kn65op.domag.R
 import io.github.kn65op.domag.dbtests.data.fillData
+import io.github.kn65op.domag.dbtests.data.mainCategory1ItemsAmountCount
+import io.github.kn65op.domag.dbtests.data.mainCategory1LimitAmount
+import io.github.kn65op.domag.dbtests.data.mainCategory1Name
 import io.github.kn65op.domag.uitests.common.activityFactory
 import io.github.kn65op.domag.uitests.common.factory
 import io.github.kn65op.domag.uitests.common.openPart
+import io.github.kn65op.domag.uitests.common.viewHasChildWithText
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -37,9 +42,13 @@ class ShortagePartTestSuite {
     private fun goToShortage() {
         openPart(R.id.nav_shortage)
     }
+    private fun assertCategoryInContents(name: String, amount: FixedPointNumber, limit: FixedPointNumber) {
+        viewHasChildWithText(R.id.fragment_shortage_recycler_view, name)
+        viewHasChildWithText(R.id.fragment_shortage_recycler_view, "${amount.toString()}/${limit.toString()}")
+    }
 
     @Test
     fun shouldShowShortageItems() {
-
+        assertCategoryInContents(mainCategory1Name, mainCategory1ItemsAmountCount, mainCategory1LimitAmount)
     }
 }
