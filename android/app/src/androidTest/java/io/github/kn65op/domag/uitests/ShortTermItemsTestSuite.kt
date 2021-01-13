dc189af5499b379dff0e88b7d97a4d4a1e8b889a
Tomasz Drzewiecki <199537+kn65op@users.noreply.github.com>
2021-01-13 19:34:51 +0100

    Update deps
    Add DI to ShortTermItemsTestSuite

--- android/app/src/androidTest/java/io/github/kn65op/domag/uitests/ShortTermItemsTestSuite.kt
+++ android/app/src/androidTest/java/io/github/kn65op/domag/uitests/ShortTermItemsTestSuite.kt
@@ -1,9 +1,13 @@
 package io.github.kn65op.domag.uitests
 
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
 import io.github.kn65op.domag.R
+import io.github.kn65op.domag.application.modules.SqlDatabaseModule
+import io.github.kn65op.domag.data.database.database.AppDatabase
 import io.github.kn65op.domag.dbtests.data.*
 import io.github.kn65op.domag.uitests.common.*
 import org.junit.After
@@ -11,23 +15,39 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import javax.inject.Inject
 
 @RunWith(AndroidJUnit4::class)
+@UninstallModules(SqlDatabaseModule::class)
+@HiltAndroidTest
 class ShortTermItemsTestSuite {
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
     @get:Rule
     var activityRule = ActivityTestRule(activityFactory, true, true)
 
+    @Inject
+    lateinit var db: AppDatabase
+
     @Before
-    fun fillDb() {
-        val db = factory.createDatabase(ApplicationProvider.getApplicationContext())
+    fun prepareTestEnvironment() {
+        injectObjects()
+        fillDb()
+        goToShortTerm()
+    }
+
+    private fun injectObjects() {
+        hiltRule.inject()
+    }
+
+    private fun fillDb() {
         fillData(db)
         Thread.sleep(500) // WA for asynchronous DB calls
-        goToShortTerm()
     }
 
     @After
     fun clearDb() {
-        val db = factory.createDatabase(ApplicationProvider.getApplicationContext())
         db.clearAllTables()
         Thread.sleep(500) // WA for asynchronous DB calls
     }
