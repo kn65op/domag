e0fae674dd838e0e4cbbc6dde338b0e95e3eb211
kn65op <199537+kn65op@users.noreply.github.com>
2021-02-03 17:03:05 +0100

    Add data model

---
+++ android/app/src/main/java/io/github/kn65op/domag/data/model/Category.kt
@@ -0,0 +1,15 @@
+package io.github.kn65op.domag.data.model
+
+import io.github.kn65op.android.lib.type.FixedPointNumber
+import io.github.kn65op.domag.utils.types.HasUid
+
+data class Category(
+    override val uid: Int? = null,
+
+    val name: String,
+    val unit: String,
+    val minimumDesiredAmount: FixedPointNumber,
+    val parent: Category?,
+    val children: List<Category>,
+    val items: List<Item>,
+) : HasUid
