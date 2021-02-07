31f10297c74fef727cba6c1786c079dd6fb623c4
kn65op <199537+kn65op@users.noreply.github.com>
2021-02-07 17:15:21 +0100

    Add Flow versions to CategoryDao

--- android/app/src/androidTest/java/io/github/kn65op/domag/dbtests/CategoryDatabaseTest.kt
+++ android/app/src/androidTest/java/io/github/kn65op/domag/dbtests/CategoryDatabaseTest.kt
@@ -6,6 +6,7 @@ import io.github.kn65op.domag.dbtests.common.DatabaseTest
 import io.github.kn65op.domag.dbtests.common.getFromLiveData
 import io.github.kn65op.domag.dbtests.data.*
 import io.github.kn65op.domag.matchers.isEqualRegardlessId
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.*
 import org.hamcrest.Matcher
@@ -15,7 +16,7 @@ import org.hamcrest.collection.IsArrayContainingInAnyOrder.arrayContainingInAnyO
 import org.junit.Before
 import org.junit.Test
 
-open class CategoryDatabaseTest  : DatabaseTest() {
+open class CategoryDatabaseTest : DatabaseTest() {
     private lateinit var categoryDao: CategoryDao
     private val matchCategoryStartingWithName: Matcher<Array<Category>> =
         arrayContainingInAnyOrder(
@@ -29,7 +30,7 @@ open class CategoryDatabaseTest  : DatabaseTest() {
     }
 
     @Test
-    fun findCategorysByExactName() {
+    fun findCategorysByExactNameLD() {
         val byName = getFromLiveData(categoryDao.findByName(category2InMainCategory1Name))
         assertThat(
             byName.toTypedArray(),
@@ -38,7 +39,16 @@ open class CategoryDatabaseTest  : DatabaseTest() {
     }
 
     @Test
-    fun findCategorysStartsWith() {
+    fun findCategorysByExactName() = runBlocking {
+        val byName = categoryDao.findByNameFlow(category2InMainCategory1Name).first()
+        assertThat(
+            byName.toTypedArray(),
+            arrayContainingInAnyOrder(isEqualRegardlessId(category2InMainCategory1.category))
+        )
+    }
+
+    @Test
+    fun findCategorysStartsWithLD() {
         val byName = getFromLiveData(categoryDao.findByName("name*"))
         assertThat(byName.size, equalTo(2))
         assertThat(
@@ -48,7 +58,17 @@ open class CategoryDatabaseTest  : DatabaseTest() {
     }
 
     @Test
-    fun getAll() {
+    fun findCategorysStartsWith() = runBlocking {
+        val byName = categoryDao.findByNameFlow("name*").first()
+        assertThat(byName.size, equalTo(2))
+        assertThat(
+            byName.toTypedArray(),
+            matchCategoryStartingWithName
+        )
+    }
+
+    @Test
+    fun getAllLD() {
         val all = getFromLiveData(categoryDao.getAll())
         assertThat(
             all.toTypedArray(),
@@ -63,7 +83,22 @@ open class CategoryDatabaseTest  : DatabaseTest() {
     }
 
     @Test
-    fun findById() {
+    fun getAll() = runBlocking {
+        val all = categoryDao.getAllFlow().first()
+        assertThat(
+            all.toTypedArray(),
+            arrayContainingInAnyOrder(
+                isEqualRegardlessId(mainCategory1.category),
+                isEqualRegardlessId(mainCategory2.category),
+                isEqualRegardlessId(category1InMainCategory1.category),
+                isEqualRegardlessId(category2InMainCategory1.category),
+                isEqualRegardlessId(mainCategory3.category)
+            )
+        )
+    }
+
+    @Test
+    fun findByIdLD() {
         val toFind = getFromLiveData(categoryDao.findByName(mainCategory1.category.name))
         assertThat(toFind.size, equalTo(1))
 
@@ -73,7 +108,17 @@ open class CategoryDatabaseTest  : DatabaseTest() {
     }
 
     @Test
-    fun findByIds() {
+    fun findById() = runBlocking {
+        val toFind = categoryDao.findByNameFlow(mainCategory1.category.name).first()
+        assertThat(toFind.size, equalTo(1))
+
+        val found = getFromLiveData(categoryDao.findWithContentsById(toFind[0].uid as Int))
+
+        assertThat(toFind[0], equalTo(found.category))
+    }
+
+    @Test
+    fun findByIdsLD() {
         val toFind = getFromLiveData(categoryDao.findByName("name*"))
 
         assertThat(toFind.size, greaterThan(1))
@@ -89,8 +134,24 @@ open class CategoryDatabaseTest  : DatabaseTest() {
     }
 
     @Test
+    fun findByIds() = runBlocking {
+        val toFind = categoryDao.findByNameFlow("name*").first()
+
+        assertThat(toFind.size, greaterThan(1))
+
+        val found =
+            getFromLiveData(categoryDao.findWithContentsById(toFind.map { it.uid as Int }
+                .toIntArray()))
+
+        assertThat(
+            found.map { it.category }.toTypedArray(),
+            matchCategoryStartingWithName
+        )
+    }
+
+    @Test
     fun delete() = runBlocking {
-        val toRemove = getFromLiveData(categoryDao.findByName(mainCategory1.category.name))
+        val toRemove = categoryDao.findByNameFlow(mainCategory1.category.name).first()
         assertThat(toRemove.size, equalTo(1))
 
         categoryDao.delete(toRemove[0])
@@ -109,7 +170,7 @@ open class CategoryDatabaseTest  : DatabaseTest() {
     }
 
     @Test
-    fun categoryWithItems() = runBlocking {
+    fun categoryWithItemsLD() = runBlocking {
         val categoryWithItems = getFromLiveData(categoryDao.findWithContentsById(3))
 
         assertThat(
@@ -119,8 +180,31 @@ open class CategoryDatabaseTest  : DatabaseTest() {
     }
 
     @Test
-    fun categoryWithChildren() {
+    fun categoryWithItems() = runBlocking {
+        val categoryWithItems = categoryDao.findWithContentsByIdFlow(3).first()
+
+        assertThat(
+            categoryWithItems.items.toTypedArray(),
+            arrayContainingInAnyOrder(item2, item4, item5)
+        )
+    }
+
+    @Test
+    fun categoryWithChildrenLD() {
         val categoryWithChildren = getFromLiveData(categoryDao.findWithContentsById(1))
+
+        assertThat(
+            categoryWithChildren.categories.toTypedArray(), arrayContainingInAnyOrder(
+                isEqualRegardlessId(category1InMainCategory1.category), isEqualRegardlessId(
+                    category2InMainCategory1.category
+                )
+            )
+        )
+    }
+
+    @Test
+    fun categoryWithChildren() = runBlocking {
+        val categoryWithChildren = categoryDao.findWithContentsByIdFlow(1).first()
 
         assertThat(
             categoryWithChildren.categories.toTypedArray(), arrayContainingInAnyOrder(
