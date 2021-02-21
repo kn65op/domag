046b6697d924451d00c53f1f70d09e63197eebdf
kn65op <199537+kn65op@users.noreply.github.com>
2021-02-21 16:55:45 +0100

    Transform also best before

--- android/app/src/main/java/io/github/kn65op/domag/data/transformations/ItemTransformations.kt
+++ android/app/src/main/java/io/github/kn65op/domag/data/transformations/ItemTransformations.kt
@@ -1,6 +1,5 @@
 package io.github.kn65op.domag.data.transformations
 
-import io.github.kn65op.android.lib.type.FixedPointNumber
 import io.github.kn65op.domag.data.model.RawItem
 import io.github.kn65op.domag.data.database.entities.Item as DbItem
 
--- android/app/src/test/java/io/github/kn65op/domag/data/transformations/ItemTransformationsTest.kt
+++ android/app/src/test/java/io/github/kn65op/domag/data/transformations/ItemTransformationsTest.kt
@@ -6,6 +6,7 @@ import io.github.kn65op.android.lib.type.FixedPointNumber
 import io.github.kn65op.domag.data.model.RawItem
 import io.github.kn65op.domag.data.database.entities.Item as DbItem
 import org.junit.Test
+import java.time.ZonedDateTime
 
 class ItemTransformationsTest {
     private val uid = 1
@@ -13,28 +14,37 @@ class ItemTransformationsTest {
     private val depotId = 23
     private val categoryId = 83
     private val description = "Description"
+    private val itemBase = DbItem(
+        uid = uid,
+        amount = amount,
+        depotId = depotId,
+        categoryId = categoryId,
+        description = description,
+        bestBefore = null
+    )
+
+    private val rawItemBase = RawItem(
+        uid = uid,
+        depotId = depotId,
+        categoryId = categoryId,
+        amount = amount,
+        description = description,
+        bestBefore = null,
+    )
 
     @Test
     fun `should transform Item to RawItem`() {
-        val itemBase = DbItem(
-            uid = uid,
-            amount = amount,
-            depotId = depotId,
-            categoryId = categoryId,
-            description = description,
-            bestBefore = null
-        )
-
-        val rawItemBase = RawItem(
-            uid = uid,
-            depotId = depotId,
-            categoryId = categoryId,
-            amount = amount,
-            description = description,
-            bestBefore = null,
-        )
-
         assertThat(itemBase.toRawItem(), equalTo(rawItemBase))
+    }
+
+    @Test
+    fun `should transform with best before`() {
+        val date = ZonedDateTime.now()
+
+        val item = itemBase.copy(bestBefore = date)
+        val rawItem = rawItemBase.copy(bestBefore = date)
+
+        assertThat(item.toRawItem(), equalTo(rawItem))
     }
 
 }
